405c93a5b812c97181e40525c28b1e68ea59adcb
sayu95 <jeongsayu95@gmail.com>
2025-12-18 18:44:48 +0900

    Setting

---
+++ PracticeSpringBoot/src/main/java/com/pratice/practicespringboot/PracticeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pratice.practicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringBootApplication.class, args);
+	}
+
+}
